dc4077e6c95f91f8f0c677d3da7f1592ca06e30f
doanyennhi <s3880599@rmit.edu.vn>
2021-12-10 15:24:59 +0700

    Log messages

--- js/firebase/firebase-login.js
+++ js/firebase/firebase-login.js
@@ -5,9 +5,12 @@ signInWithEmailAndPassword(auth, email, password)
   .then((userCredential) => {
     // Signed in
     const user = userCredential.user;
-    // ...
+    console.log(user)   // test to see if sign in works
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
+    // test if sign in works and see error
+    console.log(errorCode)
+    console.log(errorMessage)
   });
