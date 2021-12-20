56c67c0d3ae2215acb8e322a8d4cc737cd2d772a
minhthuhua72 <s3878667@rmit.edu.vn>
2021-12-20 15:55:20 +0700

    remove unused link and file

--- js/projects.js
+++
@@ -1,9 +0,0 @@
-$(".default_option").click(function () {
-  $(this).parent().toggleClass("active");
-})
-
-$(".select_ul li").click(function () {
-  var currentele = $(this).html();
-  $(".default_option li").html(currentele);
-  $(this).parents(".select_wrap").removeClass("active");
-})
