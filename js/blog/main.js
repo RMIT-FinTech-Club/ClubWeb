6d9dd91585933a43ab814700b3e1b5f2ce603610
Thu Tran <ngocanhthu20102002@gmail.com>
2021-12-29 19:17:02 +0700

    scroll to top button

--- js/blog/main.js
+++ js/blog/main.js
@@ -17,3 +17,17 @@ closeIcon.addEventListener("click", () => {
   searchIcon.parentElement.classList.remove("change");
   searchInputWrapper.classList.remove("change");
 });
+
+
+<!-- Scroll to top button -->
+var scrollToTopBtn = document.getElementById("scrollToTopBtn")
+var rootElement = document.documentElement
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+
