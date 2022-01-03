7e48df5cec0c0b442fe4450461d80dcba7485ccf
doanyennhi <s3880599@rmit.edu.vn>
2022-01-03 16:03:25 +0700

    Disable buttons when the current ep is the first or last ep in the list

--- js/main.js
+++ js/main.js
@@ -6,25 +6,38 @@ let spotifyPodcast = [
   "https://open.spotify.com/embed/episode/4g7tVZk3MJQNGZU14rMFzu?utm_source=generator"
 ]
 
-const SPOTIFY_EPS = document.querySelector("#spotify-podcast")
-SPOTIFY_EPS.setAttribute("src", spotifyPodcast[0])
-
 const PREVIOUS_BTN = document.querySelector("#prev-btn")
 const NEXT_BTN = document.querySelector("#next-btn")
+const SPOTIFY_EPS = document.querySelector("#spotify-podcast")
+// set latest podcast ep as the default ep when page first loads
+SPOTIFY_EPS.setAttribute("src", spotifyPodcast[0])
 
+// find index of the current source within the array
 const findIdx = () => {
   const src = SPOTIFY_EPS.getAttribute("src")
   return spotifyPodcast.indexOf(src)
 }
 
-PREVIOUS_BTN.addEventListener("click", () => {
+PREVIOUS_BTN.addEventListener("click", (event) => {
   const srcIdx = findIdx()
-  SPOTIFY_EPS.src=spotifyPodcast[srcIdx - 1]
+  // disable button if the current ep is the first ep in the array
+  if (srcIdx === 0) {
+    event.preventDefault()
+  } else {
+    // switch to previous ep
+    SPOTIFY_EPS.src=spotifyPodcast[srcIdx - 1]
+  }
 })
 
-NEXT_BTN.addEventListener("click", () => {
+NEXT_BTN.addEventListener("click", (event) => {
   const srcIdx = findIdx()
-  SPOTIFY_EPS.src=spotifyPodcast[srcIdx + 1]
+  // disable button if the current ep is the last ep in the array
+  if (srcIdx === spotifyPodcast.length - 1) {
+    event.preventDefault()
+  } else {
+    // switch to next ep
+    SPOTIFY_EPS.src=spotifyPodcast[srcIdx + 1]
+  }
 })
 
 //Podcast in Homepage starts here
